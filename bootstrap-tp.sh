cff03615731e32c1fb0a624bd3eae01d653f53b0
Larkin Nickle <me@larbob.org>
2019-02-25 11:44:42 -0500

    Improve bootstrap script (git config, echo on step, etc.)

--- bootstrap-tp.sh
+++ bootstrap-tp.sh
@@ -4,10 +4,19 @@ if [ $(id -u) != 0 ]; then
 	echo "You must be root to run this script."
 	exit 1
 fi
+echo "Making necessary dirs"
 mkdir -p ~/.local/bin
 mkdir -p ~/Backgrounds
 mkdir -p /etc/X11/xorg.conf.d
+echo "Removing ~/.bashrc"
 rm ~/.bashrc
+echo "Copying TearFree Xorg config"
 cp 20-intel.conf /etc/X11/xorg.conf.d
+echo "Installing packages"
 sudo apt install xorg feh stow i3 tmux firefox vim network-manager -y
+echo "Stowing configs"
 stow Backgrounds Xorg-tp bash-tp i3 scripts tmux vim
+echo "Git config"
+git config --global user.name "Larkin Nickle"
+git config --global user.email "me@larbob.org"
+git config --global credential.helper store
