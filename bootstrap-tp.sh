e45b23c3dc4897f856f92c543a9960b8efe35c80
Larkin Nickle <me@larbob.org>
2019-02-25 16:58:44 -0500

    Fix bg, fix bootstrap

--- bootstrap-tp.sh
+++ bootstrap-tp.sh
@@ -5,15 +5,17 @@ if [ $(id -u) != 0 ]; then
 	exit 1
 fi
 echo "Making necessary dirs"
-mkdir -p ~/.local/bin
-mkdir -p ~/Backgrounds
+mkdir -p /home/$USER/.local/bin
+mkdir -p /home/$USER/Backgrounds
 mkdir -p /etc/X11/xorg.conf.d
 echo "Removing ~/.bashrc"
-rm ~/.bashrc
+if [ -ne /home/$USER/.bashrc ]; then
+	rm /home/$USER/.bashrc
+fi
 echo "Copying TearFree Xorg config"
 cp 20-intel.conf /etc/X11/xorg.conf.d
-echo "Installing packages"
+echo "Updating apt and installing packages"
 apt install xorg feh stow i3 tmux firefox vim network-manager golang-go pulseaudio alsa-utils vlc python3-pip -y
 echo "Stowing configs"
-stow Backgrounds Xorg-tp bash-tp i3 scripts tmux vim git
+stow bgs Xorg-tp bash-tp i3 scripts tmux vim git
 echo "Done. Note: remove your ethernet interface from /etc/network/interfaces if you want it to be managed by Network Manager."
