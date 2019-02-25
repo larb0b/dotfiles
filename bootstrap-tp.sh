f47b7a1c88b8af2cf5f96d796db780da82924769
Larkin Nickle <me@larbob.org>
2019-02-25 19:36:08 +0000

    bootstrap-tp.sh: Add note on network interface, install pulseaudio and alsa-utils

--- bootstrap-tp.sh
+++ bootstrap-tp.sh
@@ -13,10 +13,11 @@ rm ~/.bashrc
 echo "Copying TearFree Xorg config"
 cp 20-intel.conf /etc/X11/xorg.conf.d
 echo "Installing packages"
-apt install xorg feh stow i3 tmux firefox vim network-manager golang-go -y
+apt install xorg feh stow i3 tmux firefox vim network-manager golang-go pulseaudio alsa-utils -y
 echo "Stowing configs"
 stow Backgrounds Xorg-tp bash-tp i3 scripts tmux vim
 echo "Configuring git"
 git config --global user.name "Larkin Nickle"
 git config --global user.email "me@larbob.org"
 git config --global credential.helper store
+echo "Done. Note: remove your ethernet interface from /etc/network/interfaces if you want it to be managed by Network Manager."
