337a9f5014cf3016216e7c46b9b0715ca6e17b60
Larkin Nickle <me@larbob.org>
2019-02-25 17:10:51 -0500

    bootstrap-tp.sh: add mercurial

--- bootstrap-tp.sh
+++ bootstrap-tp.sh
@@ -16,7 +16,7 @@ echo "Copying TearFree Xorg config"
 cp 20-intel.conf /etc/X11/xorg.conf.d
 echo "Updating apt and installing packages"
 apt update
-apt install xorg xinput feh stow i3 tmux firefox vim network-manager golang-go pulseaudio alsa-utils vlc python3-pip build-essential -y
+apt install xorg xinput feh stow i3 tmux firefox vim network-manager golang-go pulseaudio alsa-utils vlc python3-pip build-essential mercurial -y
 echo "Stowing configs"
 stow bgs Xorg-tp bash-tp i3 scripts tmux vim git
 echo "Done. Note: remove your ethernet interface from /etc/network/interfaces if you want it to be managed by Network Manager."
