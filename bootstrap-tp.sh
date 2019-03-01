03d06e015e0a93e86d24bc1b8982cdc3efaf89d6
Larkin <me@larbob.org>
2019-02-28 23:06:27 -0500

    bootstrap-tp.sh: ugly sudo loop...

--- bootstrap-tp.sh
+++ bootstrap-tp.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -eu
-if [ $(id -u) != 0 ]; then
-	echo "You must be root to run this script."
-	exit 1
-fi
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo "Making necessary dirs"
 mkdir -p /home/$SUDO_USER/.local/bin
 mkdir -p /home/$SUDO_USER/.local/man/man1
@@ -14,22 +12,24 @@ if [ ! -f /home/$SUDO_USER/.bashrc ]; then
 	rm /home/$SUDO_USER/.bashrc
 fi
 echo "Copying TearFree Xorg config"
-cp 20-intel.conf /etc/X11/xorg.conf.d
+sudo cp 20-intel.conf /etc/X11/xorg.conf.d
 echo "Updating apt and installing packages"
-apt update
-apt install xorg xinput feh stow i3 tmux firefox vim network-manager golang-go pulseaudio alsa-utils vlc python3-pip build-essential mercurial htop compton -y
+sudo apt update
+sudo apt install xorg xinput feh stow i3 tmux firefox vim network-manager golang-go pulseaudio alsa-utils vlc python3-pip build-essential mercurial htop compton -y
 echo "Replacing /etc/network/interfaces"
-cp interfaces /etc/network/interfaces
+sudo cp interfaces /etc/network/interfaces
 echo "Installing dependencies for Plan 9 goodness"
-apt install libx11-dev libxext-dev libxt-dev xorg-dev
+sudo apt install libx11-dev libxext-dev libxt-dev xorg-dev
 echo "Cloning, building, and installing plan9port"
-git clone https://github.com/9fans/plan9port /usr/local/plan9
-( cd "/usr/local/plan9" && ./INSTALL )
+sudo git clone https://github.com/9fans/plan9port /usr/local/plan9
+sudo bash -c 'cd "/usr/local/plan9" && ./INSTALL'
 echo "Cloning, building, and installing drawterm"
 hg clone https://code.9front.org/hg/drawterm /tmp/drawterm
-( cd "/tmp/drawterm" && CONF=unix make && mv drawterm /home/$SUDO_USER/.local/bin/drawterm && mv drawterm.1 /home/$SUDO_USER/.local/man/man1/drawterm.1 )
+sudo bash -c 'cd "/tmp/drawterm" && CONF=unix make && mv drawterm /home/$SUDO_USER/.local/bin/drawterm && mv drawterm.1 /home/$SUDO_USER/.local/man/man1/drawterm.1'
 echo "Stowing configs"
-stow bgs Xorg-tp bash-tp i3 scripts tmux vim git
+stow bgs Xorg-tp bash-tp i3 tmux vim git
+echo "Installing pfirefox script"
+cp scripts/pfirefox /home/$SUDO_USER/.local/bin/pfirefox
 echo "Removing temporary files"
 rm -rf /tmp/drawterm
 echo "Done."
