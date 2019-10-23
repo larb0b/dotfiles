8b1e5e105df1833c72a8ff32397f647e972ad548
Larkin Nickle <me@larbob.org>
2019-10-23 12:59:17 -0400

    st/config.h: Add forcemousemod for latest st

--- st/config.h
+++ st/config.h
@@ -192,6 +192,13 @@ static unsigned int mousebg = 0;
 static unsigned int defaultattr = 11;
 
 /*
+ * Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
+ * Note that if you want to use ShiftMask with selmasks, set this to an other
+ * modifier, set to 0 to not use it.
+ */
+static uint forcemousemod = ShiftMask;
+
+/*
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
